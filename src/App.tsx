668c1ee14e6f450aa6c049a4aa521ef306f95797
masterghost2002 <rakeshdhariwal61@gmail.com>
2024-05-06 08:13:22 -0400

    [Added] prettier and linting

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
-export default function App(){
-  return (<div>
-    
-  </div>)
+import { Route, Routes } from 'react-router-dom';
+import { RootLayout } from './layouts/root.layout';
+import HomePage from './pages/home.page';
+export default function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<RootLayout />}>
+        <Route index element={<HomePage />} />
+      </Route>
+    </Routes>
+  );
 }
